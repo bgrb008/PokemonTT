d6b18dfee42d91e142681eaad93485aad4a4fcc7
bgrb008 <bgrb008@gmail.com>
2026-05-13 20:29:14 +0000

    Updated ui/added region bonus

--- app.js
+++ app.js
@@ -47,12 +47,15 @@ const classes = {
          name: "Pokemon Master",
 
          stats: {
-             battletactics: 2,
+             battletactics: 3,
              bonding: 2,
              perception: 1,
-             command: 1,
+             command: 2,
              inspiration: 1,
-             pokemonlore: 0,
+             pokemonlore: 1,
+             tracking: 1,
+             survival: -1,
+             catching: 2,
          },
 
          abilities: [
@@ -63,20 +66,21 @@ const classes = {
             },
 
              {
-             name: "Master's Switch",
-             description: "Trainer may switch there active pokemon with out use of an action",
+             name: "Masters Switch",
+             description: "Trainer may switch their active pokemon without use of an action",
              cooldown: "Once per Battle",
              },
 
              {
-             name: "Clutch Syncronization",
-             description: "when your active pokemon has 30% or less hp, add 1d6 to all rolls and +2 to AC or defense",
-             cooldown: "duration 2 turns, once per battle",
+             name: "Clutch Synchronization",
+             description: "When your active pokemon has 30% or less hp, add 1d6 to all rolls and +2 to AC or defense",
+             cooldown: "Duration 2 turns, once per battle",
              },
 
              {
              name: "Bond Stability",
-             description: "when commanding pokemon +2 to all bond checks, reduce failure chance reroll 1s once per battle",
+             description: "When commanding pokemon +2 to all bond checks, reduce failure chance reroll 1s",
+                cooldown: "Once per battle"
              },
          ]
         },
@@ -88,34 +92,37 @@ const classes = {
             catching: 3,
             tracking: 2,
             perception: 1,
-            survival: 1,
+            survival: -1,
             pokemonlore: 1,
-            bonding: -1
+            bonding: 1,
+            battletactics: -1,
+            empathy: 2,
+            inspiration: 1,
         },
 
         abilities: [
             {
             name: "Efficient Catcher",
-            description: "when catching a wild pokemon, roll 1d20 + tracking , if wild pokemon is below 50% hp, add 1d6 roll, roll with advantage on first catching roll",
-            cooldown: "N/A",
+            description: "When catching a wild pokemon, roll 1d20 + tracking , if wild pokemon is below 50% hp, add 1d6 roll, roll with advantage on first catching roll",
+            cooldown: "Twice per long rest",
             },
 
             {
             name: "Field Awareness",
-            description: "when entering a wild area, roll 1d20 + tracking, if roll is more than a dc15 reveal a rare spawn",
-            cooldown: "once per wild area",
+            description: "When entering a wild area, roll 1d20 + tracking, if roll is more than a dc15 reveal a rare spawn",
+            cooldown: "Once per wild area",
             },
 
             {
-            name: "Shiny Sense",
-            description: "when encountering a wild pokemon, roll 1d20 if dc18 or higher, rare varient detected",
-            cooldown: "upon successful encounter +2 to catching rolls until wnd of encounter",
+            name: "Rare Feelings",
+            description: "When encountering a wild pokemon, roll 1d20 if dc18 or higher, rare encounter detected",
+            cooldown: "Upon successful encounter +2 to catching rolls until end of encounter",
             },
 
             {
             name: "Lucky Encounter",
-            description: "fortune seems to guide you, reroll failed encounter or catching roll",
-            cooldown: "once per ecounter",
+            description: "Fortune seems to guide you, upon succesful catch, roll 1d6 bonus xp",
+            cooldown: "Once per long rest",
             },
             
         ]
@@ -129,34 +136,38 @@ const classes = {
             bonding: 3,
             empathy: 2,
             pokemonlore: 1,
-            inspiration: 1,
+            inspiration: 2,
             command: 1,
-            catching: -1,
+            catching: 1,
+            perception: 1,
+            battletactics: -1,
+            survival: -1,
+            
         },
 
         abilities: [
             {
             name: "Nurturing care",
-            description: "your care for your pokemon is unmatched, roll 1d6 for addition healing during a short rest",
-            cooldown: "once per short rest",
+            description: "Your care for your pokemon is unmatched, roll 1d6 for addition healing during a short rest",
+            cooldown: "Once per short rest",
             },
 
             {
             name: "Evolution Insight",
-            description: "when evolving a pokemon, roll 1d20 + bonding vs dc15, if successful roll 1d4 to reduce evolution level",
-            cooldown: "once per evolution"
+            description: "When evolving a pokemon, roll 1d20 + bonding vs dc15, if successful roll 1d4 to reduce evolution level",
+            cooldown: "Once per evolution"
             },
 
             {
             name: "Calm Presence",
-            description: "when status effect is applied to your pokemon, roll 1d20 + empathy vs dc15, if successful remove status effect",
+            description: "When status effect is applied to your pokemon, roll 1d20 + empathy vs dc15, if successful remove status effect",
             cooldown: "once per battler",
             },
 
             {
             name: "Nature Boost",
-            description: "when in a wild area, roll 1d20 + empathy vs dc15, if successful add 1d6 to a healing roll",
-            cooldown: "once per wild area",
+            description: "When in a wild area, roll 1d20 + empathy vs dc15, if successful add 1d6 to a healing roll",
+            cooldown: "Once per wild area",
             },
         ]
         },
@@ -169,33 +180,36 @@ const classes = {
             command: 2,
             perception: 2,
             inspiration: 1,
-            bonding: -1,
+            bonding: 1,
             survival: -1,
+            catching: 1,
+            technology: 1,
+            empathy: 1,
         },
 
         abilities: [
             {
             name: "Prediction Window",
-            description: "when entering a battle, roll 1d20 + perception vs enemy hidden 1d20 roll + battle tactics, if successfil reveal enemy's next move",
+            description: "When entering a battle, roll 1d20 + perception vs enemy hidden 1d20 roll + battle tactics, if successfil reveal enemy's first move",
             cooldown: "once at beginning of turn",
             },
 
             {
             name: "Finisher Instinct",
-            description: "when enemy pokemon is less than 25% hp, add 1d8 damage to all attack rolls, if roll is 18 or higher, automatic critical hit",
-            cooldown: "once per battler",
+            description: "When enemy pokemon is less than 25% hp, add 1d8 damage to all attack rolls, if roll is 18 or higher, automatic critical hit",
+            cooldown: "Once per battler",
             },
 
             {
             name: "Tactical Edge",
-            description: "when entering a battle gain the advantage on your first 2 turns, +2 on all rolls and +1 to AC",
-            cooldown: "once per battle, twice per long rest",
+            description: "When entering a battle gain the advantage on your first 2 turns, +2 on all rolls and +1 to AC",
+            cooldown: "Twice per long rest",
             },
 
             {
             name: "Pressure Play",
-            description: "roll penalty for enemy, if battle master is winning the match enemy rolls with disadvantage and take -2 to AC",
-            cooldown: "once per battle",
+            description: "Roll penalty for enemy, if battle master is winning the match enemy rolls with disadvantage and take -2 to AC",
+            cooldown: "Once per battle",
             },
         ]
        
@@ -207,35 +221,38 @@ const classes = {
         stats: {
             pokemonlore: 3,
             technology: 3,
-            perception: 1,
-            battletactics: 1,
+            perception: 2,
+            battletactics: -1,
             survival: 1,
             catching: 1,
+            bonding: 1,
+            command: -1,
+            inspiration: 1,
         },
 
         abilities: [
 
             {
             name: "Scan Analysis",
-            description: "when encountering a new pikemon not in your pokedex, roll 1d20 + pokemonlore vs dc15, if successful reveal ability, resistance, and weakness",
-            cooldown: "once per encounter",
+            description: "When encountering a new pokemon not in your pokedex, roll 1d20 + technology vs dc15, if successful reveal ability, resistance, and weakness",
+            cooldown: "Twice per long rest",
             },
 
             {
             name: "Data Advantage",
-            description: "when encountering a pokemon that is in you pokedex, roll with arvantage on all rolls",
-            cooldown: "twice per long rest",
+            description: "When encountering a pokemon that is in your pokedex, roll with arvantage on all rolls",
+            cooldown: "Twice per long rest",
             },
 
             {
             name: "Field study",
-            description: "wehn in the wild area, after a successful encounter, roll 1d20 if 12 or higher roll 1d8 to gain extra xp",
-            cooldown: "once per encounter",
+            description: "When in the wild area, after a successful encounter, roll 1d20 if 15or higher roll 1d8 to gain extra xp",
+            cooldown: "Once per encounter",
             },
 
             {
-            name: "tech Synergy",
-            description: "when using an item roll 1d6 to add to strength of item or reroll failed item activation",
+            name: "Tech Synergy",
+            description: "When using an item roll 1d6 to add to strength of item or reroll failed item activation",
             cooldown: "once per item",
             },
         ]
@@ -251,32 +268,35 @@ const classes = {
             perception: 1,
             pokemonlore: 1,
             survival: -1,
+            catching: 2,
+            battletactics: 2,
+            technology: 2,
         },
 
         abilities: [
 
             {
             name: "Crowd Hype",
-            description: "during battle if player rolls a k/o or critical hit, roll 1d20 + inspiration vs dc15, if successeful stack 1d6 to all damage rolls",
-            cooldown: "max of 4 stacks and 1 time per long rest",
+            description: "During battle if player rolls a k/o or critical hit, roll 1d20 + inspiration vs dc15, if successeful stack 1d6 to all damage rolls, if failed stacks reset",
+            cooldown: "Max of 4 stacks and once per battle",
             },
 
             {
             name: "Charisma Boost",
-            description: "at the end of battle, if successful, roll 1d20 + inspiration, if 15 or higher gain 1d6 xp, if you roll 18 or higher roll 2d6 xp",
-            cooldown: "once per battle",
+            description: "At the end of battle, if influencer wins, roll 1d20 + inspiration, if 15 or higher gain 1d6 xp, if you roll 18 or higher roll 2d6 xp",
+            cooldown: "Once per battle",
             },
 
             {
             name: "Trend Setter",
-            description: "when beginning a new battle, roll 1d20 + inspiration vs dc15, if successful gain advantage + 1d4 on first attack",
-            cooldown: "once per battle",
+            description: "When beginning a new battle, roll 1d20 + inspiration vs dc15, if successful gain advantage + 1d4 on first attack",
+            cooldown: "Once per battle",
             },
 
             {
             name: "Fame",
-            description: "the more you win the more famous you become, add 1d4 per win to all xp rolls",
-            cooldown: "if you lose a battle, all stacks are removed",
+            description: "The more you win the more famous you become, add 1d4 per win to all xp rolls",
+            cooldown: "If you lose a battle, all stacks are removed",
             },
         ]
         },
@@ -289,33 +309,36 @@ const classes = {
             bonding: 2,
             battletactics: 1,
             perception: 1,
-            command: 1,
+            command: 2,
             catching: -1,
+            survival: -1,
+            pokemonlore: 1,
+            tracking: -1,
         },
 
         abilities: [
             {
             name: "Stage Pressence",
-            description: "during a contest battle, on your turn roll 1d20+ inspiration vs dc15, if successeful gain a combo bonus of 1d6 additional damage",
-            cooldown: "once per battle",
+            description: "During a contest battle, on your turn roll 1d20 + inspiration vs dc15, if successeful gain a combo bonus of 1d6 additional damage",
+            cooldown: "Once per battle",
             },
 
             {
             name: "Flow State",
-            description: "at the beginning of your turn, roll 1d20 + bonding vs dc15, if successeful gain 1d4 to all damage rolls, can stack up to 4 times",
-            cooldown: "if fail stacks reset, once per long rest",
+            description: "At the beginning of your turn, roll 1d20 + bonding vs dc15, if successeful gain 1d4 to all damage rolls, can stack up to 4 times",
+            cooldown: "If fail stacks reset, once per battle",
             },
 
             {
             name: "Audience Effect",
-            descriptions: "when performing a contest move, draw inspiration from the crowd, roll 1d20 + inspiration vs dc15, if successful add 1d6 in bonus xp",
-            cooldown: "once per contest",
+            descriptions: "When performing a contest move, draw inspiration from the crowd, roll 1d20 + inspiration vs dc15, if successful add 1d6 in bonus damage",
+            cooldown: "Once per contest",
             },
 
             {
             name: "Graceful Recovery",
-            description: "during a contest battle, if your pokemon gets knocked out, roll 1d20 + bonding if 15 or higher, pokemon gains 1d6 of hp",
-            cooldown: "once per long rest",
+            description: "During a contest battle, if your pokemon gets knocked out, roll 1d20 + bonding if 15 or higher, pokemon gains 1d6 of hp, if 18 or higher gain 2d6",
+            cooldown: "Once per long rest",
             },
         ]
         },
@@ -325,36 +348,40 @@ const classes = {
 
         stats: {
             survival: 3,
-            tracking: 3,
+            tracking: 2,
             empathy: 1,
             perception: 1,
-            technology: 1,
+            technology: -1,
             battletactics: -1,
+            catching: 1,
+            pokemonlore: 1,
+            bonding: 2,
+            
         },
 
         abilities: [
             {
             name: "Wild Sense",
-            description: "when entering a wild are roll 1d20 + tracking vs dc15, if successful reveal a rare spawn",
-            cooldown: "once per wild area",
+            description: "When entering a wild area roll 1d20 + tracking vs dc15, if successful reveal a rare spawn",
+            cooldown: "Once per wild area",
             },
 
             {
             name: "Nature Bond",
-            description: "when encountering a wild pokemon, roll 1d20 + empathy vs dc15 if successful, skip battle and add 1d4 to catch roll",
-            cooldown: "twice per long rest"
+            description: "When encountering a wild pokemon, roll 1d20 + empathy vs dc15 if successful, skip battle and add 1d4 to catch roll",
+            cooldown: "Twice per long rest"
             },
 
             {
             name: "Environmental Adaptation",
-            description: "when in a wild area or gym gain 1d6 to all damage rolls, based on environment( ie. grass, water, etc)",
-            cooldown: "once per battle",
+            description: "When in a wild area or gym gain 1d6 to all damage rolls, based on environment( ie. grass, water, etc)",
+            cooldown: "Once per battle or encounter",
             },
 
             {
             name: "Guardian Instinct",
-            description: "when in a wild area roll 1d20 + perception vs dc 15, if successful divert 25% of allys damage to yourself",
-            cooldown: "once per wild area",
+            description: "When in a wild area roll 1d20 + perception vs dc 15, if successful divert 25% of allys damage to yourself",
+            cooldown: "Once per wild area",
             },
         ]
         },
